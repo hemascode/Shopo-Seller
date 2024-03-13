3c6509e121d6e851eb434a52ff3d5ff0d2f8d906
mohammadthariq@26 <thariq.md.ans@gmail.com>
2024-03-13 13:07:54 +0530

    Seller inventory setup

--- src/Components/Inventory/index.jsx
+++ src/Components/Inventory/index.jsx
@@ -1,8 +1,10 @@
 import { Breadcrumb } from "@/common/Breadcrumb";
+import { PageHeader } from "@/common/PageHeader";
 
 export const Inventory = () => {
   return (
     <>
+      <PageHeader title="Inventory" />
       <Breadcrumb currentPage={"Inventory"} serachEnable />
     </>
   );
